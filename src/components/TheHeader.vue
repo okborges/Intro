375eadac4f88a64b6f6ff190dbb01ff8d95c6170
okborges <rafaelborges.av@gmail.com>
2023-03-30 15:24:11 -0300

    bind style

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,3 +1,22 @@
 <template>
-  <header>Header</header>
+  <header :class="{ texto: true, tamanho: estaAtivo }">Header</header>
+  <button @click="ativar()">ativar</button>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+let estaAtivo = ref(true)
+function ativar() {
+  estaAtivo.value = !estaAtivo.value
+}
+</script>
+
+<style lang="scss">
+.texto {
+  color: aquamarine;
+}
+.tamanho {
+  font-size: 60px;
+}
+</style>
